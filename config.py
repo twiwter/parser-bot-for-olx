d6a96b149e66b5c7424a54db391c80bd620a176b
Alexander <alexander.karadyaur@gmail.com>
2021-07-28 18:59:00 +0200

    Fixed

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from telebot.types import InlineKeyboardButton
 
-BOT_TOKEN = '1874638072:AAGwUF6R-ZbMlXsCgZo4g6POb-3VlDy0gfc'  # Bot token
+BOT_TOKEN = '1874638072:AAGqNKBzWcAXbd7URg5uCjtoGu5iV-t34_s'  # Bot token
 
 BOT_SETTINGS = {
     'language': (['🇷🇺 Русский', 'ru'], ['🇺🇦 Українська', 'ua']),
