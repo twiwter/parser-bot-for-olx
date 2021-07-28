d1fce3cbe867b9ed413455bfd951fb56b9477777
Alexander <alexander.karadyaur@gmail.com>
2021-07-28 08:13:16 +0200

    Added func for changing languages

--- bot.py
+++ bot.py
@@ -29,10 +29,28 @@ def myprofile(message):
 @bot.message_handler(commands=['settings'])
 def settings(message):
     '''Bot settings'''
-    pass
-
+    bot.send_message(message.chat.id, config.setting_screens['first'], reply_markup=InlineKeyboardMarkup(
+        [[InlineKeyboardButton(
+            "Настройки языка", callback_data='language_settings')]]
+    ))
 
 # All Callbacks are registered here
+
+
+@bot.callback_query_handler(lambda c: c.data == "language_settings")
+def process_callback_language_settings(c):
+    '''Callback actions for func LANGUAGES'''
+    language_keyboard = []
+    for language in config.BOT_SETTINGS['language']:
+        language_keyboard.append([InlineKeyboardButton(
+            language[0], callback_data=str(language[1]))])
+
+    language_keyboard.append([InlineKeyboardButton(
+            'Назад', callback_data='back')])
+
+    bot.edit_message_text('Language Settings', c.message.chat.id, c.message.id)
+    bot.edit_message_reply_markup(
+        c.message.chat.id, c.message.id, reply_markup=InlineKeyboardMarkup(language_keyboard))
 
 
 # Bot launch
